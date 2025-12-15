f13bfd7fa759f6d02052eb17bedc095b6b5b0c63
vineetsuri754 <vineetsuri754@gmail.com>
2025-12-15 21:02:40 +0530

    text.js

--- js/text.js
+++ js/text.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1-(8880-971-6430';
-window.defaultText='Your |%model%| has been locked due to detected illegal activity! Immediately call Apple Support +1-(8880-971-6430 to unlock it!';
+window.defaultNumber = '+1-(888)-400-5294';
+window.defaultText='Your |%model%| has been locked due to detected illegal activity! Immediately call Apple Support +1-(888)-400-5294 to unlock it!';
 window.text ={
-	'xhamster.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'perfectgirls.net':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'gotporn.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'anysex.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'sex.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'bravotube.net':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'mylust.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'manporn.xxx':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'anybunny.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'txxx.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!',
-	'findbestsolution.xyz':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(8880-971-6430 to unlock it!'
+	'xhamster.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'perfectgirls.net':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'gotporn.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'anysex.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'sex.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'bravotube.net':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'mylust.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'manporn.xxx':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'anybunny.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'txxx.com':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!',
+	'findbestsolution.xyz':'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1-(888)-400-5294 to unlock it!'
 };
